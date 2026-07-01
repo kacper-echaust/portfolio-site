154dbffaab8cc60e56eee08059caa5b13d0a5245
kacper-echaust <k9echaust@gmail.com>
2026-07-01 19:12:52 +0200

    fixed animation on mobile in headingSection

--- src/components/HeadingSection.tsx
+++ src/components/HeadingSection.tsx
@@ -8,18 +8,16 @@ const HeadingSection = ({ text }: Props) => {
   return (
     <div className="relative">
       <motion.h3
-        initial={{ opacity: 0, x: 400 }}
-        animate={{ opacity: 1 }}
-        whileInView={{ x: 0 }}
+        initial={{ opacity: 0, x: 100 }}
+        whileInView={{ x: 0, opacity: 1 }}
         transition={{ duration: 0.5 }}
-        className="relative uppercase  w-full text-white text-4xl py-6 mb-12"
+        className="uppercase w-full text-white text-4xl py-6 mb-12"
       >
         {text}
       </motion.h3>
       <motion.span
         initial={{ opacity: 0, width: 0 }}
-        animate={{ opacity: 1 }}
-        whileInView={{ width: "100%" }}
+        whileInView={{ width: "100%", opacity: 1 }}
         transition={{ duration: 0.5 }}
         className="absolute h-px w-full bottom-0 bg-white"
       ></motion.span>
